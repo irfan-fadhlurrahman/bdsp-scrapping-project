35b16b194279aac9bc08cc1b2bfde9797de54459
Muhammad Irfan Fadhlurrahman <85064637+irfan-fadhlurrahman@users.noreply.github.com>
2023-10-29 19:06:36 +0700

    Update refactor.py
    Add harga pangan class

--- src/refactor.py
+++ src/refactor.py
@@ -1,1 +1,15 @@
-# Test
+import pytx
+
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class HargaPanganRataRata:
+  url: str
+  user_id: str
+  db_conn: DatabaseConnection
+  file_handler: FileHandler
+  table: SQLTableMetadata
+  current_date: datetime
+  
+  
